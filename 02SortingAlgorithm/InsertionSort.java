08f5a8cd32d2637fe86552369eaf325987f4ed91
shaikshoaib <shaikshoaib820@gmail.com>
2025-01-07 08:03:37 +0530

    sorting

--- 02SortingAlgorithm/InsertionSort.java
+++ 02SortingAlgorithm/InsertionSort.java
@@ -1,30 +1,25 @@
 import java.util.Arrays;
 
-public class SelectionSort {
+public class InsertionSort {
     public static int[] sort(int[] arr){
         int size = arr.length;
-        for(int i=0;i<size-1;i++){
-            int min=i;
-            for(int j=i+1;j<size;j++){
-                if(arr[min]>arr[j]){
-                    min=j;
-                }
+        for(int i=1;i<size;i++){
+            int key = arr[i];
+            int j = i-1;
+            while (j >=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
             }
-            int temp=arr[min];
-            arr[min] = arr[i];
-            arr[i]= temp;
-        }return arr;
+            arr[j+1]=key;
+           
+        } return arr;
     }
     public static void main(String[] args) {
-        
-       
         int[] arr = {5, 4, 8, 2, 1};
         System.out.println("Original Array: " + Arrays.toString(arr));
 
         sort(arr); 
         System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
+    
 }
-    
-    
-
