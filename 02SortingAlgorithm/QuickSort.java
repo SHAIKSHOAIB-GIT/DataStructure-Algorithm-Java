da75da10379ce8899b10377e654bc3d7aba36664
shaikshoaib <shaikshoaib820@gmail.com>
2025-01-23 17:44:44 +0530

    qucikSort

--- 02SortingAlgorithm/QuickSort.java
+++ 02SortingAlgorithm/QuickSort.java
@@ -1,25 +1,46 @@
 import java.util.Arrays;
 
 public class QuickSort {
-    public static void quickSort(int[] arr){
-        for (int i=1; i<arr.length;i++){
-            int key = arr[i];
-            int j = i-1;
-            while (j>=0 && arr[j]>key){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1] =key;
+    public int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i =low - 1;
+        for(int j = low; j < high; j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
 
+        return i + 1;
+    }
+    private void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            // Find the partitioning index
+            int partitionIndex = partition(arr, low, high);
 
+            // Recursively sort elements before and after partitioning index
+            quickSort(arr, low, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, high);
+        }
+    
+    }
     public static void main(String[] args) {
         int[] arr = {5, 4, 8, 2, 1};
+        int n = arr.length;
+        QuickSort sorter = new QuickSort();
         System.out.println("Original Array: " + Arrays.toString(arr));
 
-        quickSort(arr);  
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        sorter.quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
     
